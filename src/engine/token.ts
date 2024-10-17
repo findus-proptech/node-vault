aa1f3b5b01ef1812652fd6aafcd16d7b8dcaab83
Arvid Petermann <arvidpetermann@googlemail.com>
2024-10-17 08:39:00 +0100

    feat(token): roles and tidy

--- src/engine/token.ts
+++ src/engine/token.ts
@@ -93,6 +93,132 @@ export class TokenMethod extends ApiSector {
   }
 
   /**
+   * Lookup a token
+   *
+   * @link https://developer.hashicorp.com/vault/api-docs/auth/token#lookup-a-token
+   */
+  get lookup() {
+    return generateCommand({
+      method: 'POST',
+      path: '/auth/token/lookup',
+      client: this.client,
+      schema: {
+        body: z.object({
+          token: z.string()
+        }),
+        response: z.object({
+          data: z.object({
+            accessor: z.string(),
+            creation_time: z.number(),
+            creation_ttl: z.number(),
+            display_name: z.string(),
+            entity_id: z.string(),
+            expire_time: z.string().nullable(),
+            explicit_max_ttl: z.number(),
+            id: z.string(),
+            identity_policies: z.array(z.string()).optional(),
+            issue_time: z.string().optional(),
+            meta: z
+              .object({
+                username: z.string()
+              })
+              .nullable(),
+            num_uses: z.number(),
+            orphan: z.boolean(),
+            path: z.string(),
+            policies: z.array(z.string()),
+            renewable: z.boolean().optional(),
+            ttl: z.number()
+          })
+        })
+      }
+    });
+  }
+
+  /**
+   * Lookup a token (Self)
+   *
+   * @link https://developer.hashicorp.com/vault/api-docs/auth/token#lookup-a-token-self
+   */
+  get lookupSelf() {
+    return generateCommand({
+      method: 'GET',
+      path: '/auth/token/lookup-self',
+      client: this.client,
+      schema: {
+        response: z.object({
+          data: z.object({
+            accessor: z.string(),
+            creation_time: z.number(),
+            creation_ttl: z.number(),
+            display_name: z.string(),
+            entity_id: z.string(),
+            expire_time: z.string().nullable(),
+            explicit_max_ttl: z.number(),
+            id: z.string(),
+            identity_policies: z.array(z.string()).optional(),
+            issue_time: z.string().optional(),
+            meta: z
+              .object({
+                username: z.string()
+              })
+              .nullable(),
+            num_uses: z.number(),
+            orphan: z.boolean(),
+            path: z.string(),
+            policies: z.array(z.string()),
+            renewable: z.boolean().optional(),
+            ttl: z.number()
+          })
+        })
+      }
+    });
+  }
+
+  /**
+   * Lookup a token (Accessor)
+   *
+   * @link https://developer.hashicorp.com/vault/api-docs/auth/token#lookup-a-token-accessor
+   */
+  get lookupAccessor() {
+    return generateCommand({
+      method: 'POST',
+      path: '/auth/token/lookup-accessor',
+      client: this.client,
+      schema: {
+        body: z.object({
+          accessor: z.string()
+        }),
+        response: z.object({
+          data: z.object({
+            accessor: z.string(),
+            creation_time: z.number(),
+            creation_ttl: z.number(),
+            display_name: z.string(),
+            entity_id: z.string(),
+            expire_time: z.string().nullable(),
+            explicit_max_ttl: z.number(),
+            id: z.string(),
+            identity_policies: z.array(z.string()).optional(),
+            issue_time: z.string().optional(),
+            meta: z
+              .object({
+                username: z.string()
+              })
+              .nullable(),
+            num_uses: z.number(),
+            orphan: z.boolean(),
+            path: z.string(),
+            policies: z.array(z.string()),
+            renewable: z.boolean().optional(),
+            ttl: z.number()
+          })
+        })
+      }
+    });
+  }
+
+  /**
    * Renew a token
    *
    * @link https://developer.hashicorp.com/vault/api-docs/auth/token#renew-a-token
@@ -226,82 +352,79 @@ export class TokenMethod extends ApiSector {
   }
 
   /**
-   * Lookup a token
+   * Revoke a token and orphan children
    *
-   * @link https://developer.hashicorp.com/vault/api-docs/auth/token#lookup-a-token
+   * @link https://developer.hashicorp.com/vault/api-docs/auth/token#revoke-token-and-orphan-children
    */
-  get lookup() {
+  get revokeOrphan() {
     return generateCommand({
       method: 'POST',
-      path: '/auth/token/lookup',
+      path: '/auth/token/revoke-orphan',
       client: this.client,
       schema: {
         body: z.object({
           token: z.string()
-        }),
-        response: z.object({
-          data: z.object({
-            accessor: z.string(),
-            creation_time: z.number(),
-            creation_ttl: z.number(),
-            display_name: z.string(),
-            entity_id: z.string(),
-            expire_time: z.string().nullable(),
-            explicit_max_ttl: z.number(),
-            id: z.string(),
-            identity_policies: z.array(z.string()).optional(),
-            issue_time: z.string().optional(),
-            meta: z
-              .object({
-                username: z.string()
-              })
-              .nullable(),
-            num_uses: z.number(),
-            orphan: z.boolean(),
-            path: z.string(),
-            policies: z.array(z.string()),
-            renewable: z.boolean().optional(),
-            ttl: z.number()
-          })
         })
       }
     });
   }
 
   /**
-   * Lookup a token (Self)
+   * Read token role
    *
-   * @link https://developer.hashicorp.com/vault/api-docs/auth/token#lookup-a-token-self
+   * @link https://developer.hashicorp.com/vault/api-docs/auth/token#read-token-role
    */
-  get lookupSelf() {
+  get roleRead() {
     return generateCommand({
       method: 'GET',
-      path: '/auth/token/lookup-self',
+      path: '/auth/token/roles/{{role_name}}',
+      client: this.client,
+      schema: {
+        path: z.object({
+          role_name: z.string()
+        }),
+        response: z.object({
+          request_id: z.string(),
+          lease_id: z.string().optional(),
+          lease_duration: z.number(),
+          renewable: z.boolean(),
+          data: z.object({
+            allowed_entity_aliases: z.array(z.string()),
+            allowed_policies: z.array(z.string()),
+            disallowed_policies: z.array(z.string()),
+            allowed_policies_glob: z.array(z.string()),
+            disallowed_policies_glob: z.array(z.string()),
+            explicit_max_ttl: z.number(),
+            name: z.string(),
+            orphan: z.boolean(),
+            path_suffix: z.string(),
+            period: z.number(),
+            renewable: z.boolean(),
+            token_explicit_max_ttl: z.number(),
+            token_no_default_policy: z.boolean(),
+            token_period: z.number(),
+            token_type: z.string()
+          }),
+          warnings: z.any().nullable()
+        })
+      }
+    });
+  }
+
+  /**
+   * List token role
+   *
+   * @link https://developer.hashicorp.com/vault/api-docs/auth/token#list-token-roles
+   */
+  get roles() {
+    return generateCommand({
+      method: 'LIST',
+      path: '/auth/token/roles',
       client: this.client,
       schema: {
         response: z.object({
           data: z.object({
-            accessor: z.string(),
-            creation_time: z.number(),
-            creation_ttl: z.number(),
-            display_name: z.string(),
-            entity_id: z.string(),
-            expire_time: z.string().nullable(),
-            explicit_max_ttl: z.number(),
-            id: z.string(),
-            identity_policies: z.array(z.string()).optional(),
-            issue_time: z.string().optional(),
-            meta: z
-              .object({
-                username: z.string()
-              })
-              .nullable(),
-            num_uses: z.number(),
-            orphan: z.boolean(),
-            path: z.string(),
-            policies: z.array(z.string()),
-            renewable: z.boolean().optional(),
-            ttl: z.number()
+            keys: z.array(z.string())
           })
         })
       }
@@ -309,45 +432,69 @@ export class TokenMethod extends ApiSector {
   }
 
   /**
-   * Lookup a token (Accessor)
+   * Create/Update token role
    *
-   * @link https://developer.hashicorp.com/vault/api-docs/auth/token#lookup-a-token-accessor
+   * @link https://developer.hashicorp.com/vault/api-docs/auth/token#create-update-token-role
    */
-  get lookupAccessor() {
+  get roleCreateUpdate() {
     return generateCommand({
       method: 'POST',
-      path: '/auth/token/lookup-accessor',
+      path: '/auth/token/roles/{{role_name}}',
       client: this.client,
       schema: {
-        body: z.object({
-          accessor: z.string()
+        path: z.object({
+          role_name: z.string()
         }),
-        response: z.object({
-          data: z.object({
-            accessor: z.string(),
-            creation_time: z.number(),
-            creation_ttl: z.number(),
-            display_name: z.string(),
-            entity_id: z.string(),
-            expire_time: z.string().nullable(),
-            explicit_max_ttl: z.number(),
-            id: z.string(),
-            identity_policies: z.array(z.string()).optional(),
-            issue_time: z.string().optional(),
-            meta: z
-              .object({
-                username: z.string()
-              })
-              .nullable(),
-            num_uses: z.number(),
-            orphan: z.boolean(),
-            path: z.string(),
-            policies: z.array(z.string()),
-            renewable: z.boolean().optional(),
-            ttl: z.number()
-          })
+        body: z.object({
+          name: z.string(),
+          allowed_policies: z.array(z.string()).optional(),
+          disallowed_policies: z.array(z.string()).optional(),
+          allowed_policies_glob: z.array(z.string()).optional(),
+          disallowed_policies_glob: z.array(z.string()).optional(),
+          orphan: z.boolean().optional(),
+          renewable: z.boolean().optional(),
+          path_suffix: z.string().optional(),
+          allowed_entity_aliases: z.union([z.string(), z.array(z.string())]).optional(),
+          token_bound_cidrs: z.array(z.string()).optional(),
+          token_explicit_max_ttl: z.union([z.number().nonnegative(), z.string()]).optional(),
+          token_no_default_policy: z.boolean().optional(),
+          token_num_uses: z.number().nonnegative().optional(),
+          token_period: z.union([z.number().nonnegative(), z.string()]).optional(),
+          token_type: z.string().optional()
         })
       }
+    });
+  }
+
+  /**
+   * Delete token role
+   *
+   * @link https://developer.hashicorp.com/vault/api-docs/auth/token#delete-token-role
+   */
+  get roleDelete() {
+    return generateCommand({
+      method: 'DELETE',
+      path: '/auth/token/roles/{{role_name}}',
+      client: this.client,
+      schema: {
+        path: z.object({
+          role_name: z.string()
+        })
+      }
+    });
+  }
+
+  /**
+   * Tidy tokens
+   *
+   * @link https://developer.hashicorp.com/vault/api-docs/auth/token#tidy-tokens
+   */
+  get tidy() {
+    return generateCommand({
+      method: 'POST',
+      path: '/auth/token/tidy',
+      client: this.client,
+      schema: {}
     });
   }
 }
