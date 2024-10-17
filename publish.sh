612fdeff29c7942f93da5d2de9fe963cb26876ce
Arvid Petermann <arvidpetermann@googlemail.com>
2024-10-17 05:20:47 +0100

    feat(token): wip

--- publish.sh
+++ publish.sh
@@ -4,7 +4,7 @@ FEATURE=${2}
 jq '.version |= (. | split(".") | .[2] |= tostring | .[0] + "." + .[1] + "." + (.[2] | tonumber + 1 | tostring))' package.json > tmp.$$.json && mv tmp.$$.json package.json
 git pu "feat(${1}): ${2}" || true
 pnpm build
-pnpm publish
+pnpm publish --no-git-checks
 cd ../../fp 
 pnpm install @findusproptech/node-vault@latest
 cd ../forks/node-vault
